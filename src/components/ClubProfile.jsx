8acb5360082a00f2782c7a12b843ede91ba1341b
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-05-11 19:35:03 +0200

    style club profile

--- src/components/ClubProfile.jsx
+++ src/components/ClubProfile.jsx
@@ -11,14 +11,15 @@ const ClubProfile = () => {
                 <h3>Founded on 30/08/1995</h3>
                </div>
            </Col>
-           <Col md={10}>
-            <h2>Players</h2>
+           <Col md={10} >
+           <div className=" search-sec-container me-2 mt-2 mb-2"> <h2>Players</h2>
             <Row className="scroller-club"> 
             {Array.apply(0, Array(25)).map(function (x, i) {
     return <Col> <Card  name={"Van basten"} image={"https://gianlucadimarzio.com/images/van_basten_milan_gdm.jpg?p=intextimg&s=d6e09cb71a6e2ae3835c28cf87a3f4b0"}
     nationality={"Netherlands"} club={"A.C. Milan"} position={"STR"} pac={"99"} shot={"99"}  pas={"99"} dri={"99"} def={"99"} phy={"99"}
      /> </Col>
   })}</Row>
+  </div>
            </Col>
        </Row>
     )
