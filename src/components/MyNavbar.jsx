d432b34b4fe93fff262969a2ce4cb381edaf9de2
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-05-11 19:01:55 +0200

    Update MyNavbar.jsx

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -5,7 +5,7 @@ const MyNavbar = () => {
         <>
           <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="/search">
+    <Navbar.Brand href="/club">
         <img
           alt="logo-navbar"
           src={logo}
@@ -18,7 +18,7 @@ const MyNavbar = () => {
     <Nav className="ms-auto align-items-center">
       <Nav.Link href="/me">Me</Nav.Link>
       <Nav.Link href="/favourites-players">Fav players</Nav.Link>
-      <Nav.Link href="/favourites-clubs">Fav clubs</Nav.Link>
+      <Nav.Link href="/search"><i className="bi bi-search" style={{fontSize: "25px"}}></i></Nav.Link>
       <Nav.Link href="/"><i className="bi bi-door-open" style={{fontSize: "25px"}}></i></Nav.Link>
     </Nav>
     </Container>
