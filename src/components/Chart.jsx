cf9c6af55897edd78429ceada7d2fec91785aecc
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-05-13 12:49:04 +0200

    Chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,71 +1,34 @@
-import { ResponsiveRadar } from '@nivo/radar'
+import { Radar } from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
 
-export const MyResponsiveRadar = () => (
-    <ResponsiveRadar
-        data = {[
-            {
-              "taste": "fruity",
-              "chardonay": 33,
-              "carmenere": 27,
-              "syrah": 23
-            },
-            {
-              "taste": "bitter",
-              "chardonay": 56,
-              "carmenere": 111,
-              "syrah": 59
-            },
-            {
-              "taste": "heavy",
-              "chardonay": 85,
-              "carmenere": 63,
-              "syrah": 100
-            },
-            {
-              "taste": "strong",
-              "chardonay": 95,
-              "carmenere": 52,
-              "syrah": 78
-            },
-            {
-              "taste": "sunny",
-              "chardonay": 28,
-              "carmenere": 95,
-              "syrah": 115
-            }
-          ]}
-        keys={[ 'chardonay', 'carmenere', 'syrah' ]}
-        indexBy="taste"
-        valueFormat=">-.2f"
-        margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
-        borderColor={{ from: 'color' }}
-        gridLabelOffset={36}
-        dotSize={10}
-        dotColor={{ theme: 'background' }}
-        dotBorderWidth={2}
-        colors={{ scheme: 'nivo' }}
-        blendMode="multiply"
-        motionConfig="wobbly"
-        legends={[
-            {
-                anchor: 'top-left',
-                direction: 'column',
-                translateX: -50,
-                translateY: -40,
-                itemWidth: 80,
-                itemHeight: 20,
-                itemTextColor: '#999',
-                symbolSize: 12,
-                symbolShape: 'circle',
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemTextColor: '#000'
-                        }
-                    }
-                ]
-            }
-        ]}
-    />
+  ChartJS.register(
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend
+  );
+
+export const Chart = () => (
+  <Radar data={{
+    labels: ['Pac', 'Sho', 'Pas', 'Dri', 'Def', 'Phy'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [8, 9, 7, 9, 4, 8],
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+    ],
+  }}/>
 )
--- src/components/ProfileDataSection.jsx
+++ src/components/ProfileDataSection.jsx
@@ -1,10 +1,12 @@
+import { Chart } from "./Chart"
+
+
 const ProfileDataSection = () => {
     return(
         <div className="search-sec-container mb-2 mt-2">
             <h1>Test</h1>
            <div className="d-flex align-items-center">
-            <i className="bi bi-person-fill"></i>
-            <input className="ms-3 h-50" type="text" placeholder="type something..."></input>
+           <Chart/>
             </div>
         </div>
     )
