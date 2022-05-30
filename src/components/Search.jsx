b20be25f834a32ce3ea68ffa23cd2fbefb97f93f
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-05-30 17:05:27 +0200

    search section

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -19,13 +19,11 @@ const Search = () => {
             </div>
 
             <Row className="m-auto mb-4 mt-5">
-                <Col sm={6} md={6} lg={4}><SearchSection title={"Player"} icon={"bi bi-person-fill"} type={"text"}/></Col>
-                <Col sm={6} md={6} lg={4}><SearchSection title={"Position"} icon={"bi bi-bullseye"} type={"text"}/></Col>
-                <Col sm={6} md={6} lg={4}><SearchSection title={"Nationality"} icon={"bi bi-globe"} type={"text"}/></Col>
+                <Col sm={6} md={6} lg={6}><SearchSection title={"Name"} icon={"bi bi-person-fill"} type={"text"} text={"name..."}/></Col>
+                <Col sm={6} md={6} lg={6}><SearchSection title={"Position"} icon={"bi bi-bullseye"} type={"text"} text={"position..."}/></Col>
 
-                <Col sm={6} md={6} lg={4}><SearchSection title={"Age"} icon={"bi bi-calendar-week"} type={"date"}/></Col>
-                <Col sm={6} md={6} lg={4}><SearchSection title={"Country"} icon={"bi bi-globe"} type={"text"} /></Col>
-                <Col sm={6} md={6} lg={4}><SearchSection title={"Club"} icon={"bi bi-building"} type={"text"}/></Col>
+                <Col sm={6} md={6} lg={6}><SearchSection title={"Birth year"} icon={"bi bi-calendar-week"} type={"number"} min={1900} max={2022}/></Col>
+                <Col sm={6} md={6} lg={6}><SearchSection title={"Country"} icon={"bi bi-globe"} type={"text"} text={"country..."}/></Col>
             </Row>
 
            { location === "/search-club-player" ? <Button variant="dark" className="search-button m-auto p-3" onClick={()=> navigate("/search-club-result") }>Search</Button> : location === "/search-compare-player1" ? <Button variant="dark" className="search-button m-auto p-3" onClick={()=> navigate("/search-compare-result1") }>Search</Button> : location === "/search-compare-player2" ? <Button variant="dark" className="search-button m-auto p-3" onClick={()=> navigate("/search-compare-result2") }>Search</Button>:<Button variant="dark" className="search-button m-auto p-3" onClick={()=> navigate("/search-result") }>Search</Button>}
--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -1,10 +1,10 @@
-const SearchSection = ({title, icon, type}) => {
+const SearchSection = ({title, icon, type, min, max, text}) => {
     return(
         <div className="search-sec-container mb-4">
             <h1>{title}</h1>
            <div className="d-flex align-items-center">
             <i className={icon}></i>
-            <input className="ms-3 h-50" type={type} placeholder="type something..."></input>
+            <input className="ms-3 h-50" type={type} min={min} max={max} placeholder={text}></input>
             </div>
         </div>
     )
